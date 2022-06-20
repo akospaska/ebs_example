aa4cec13ce0fd413d18f159ff75998dce33fbf37
Ákos <akos.paska@risingstack.com>
2022-06-20 14:38:14 +0200

    serve the latest commit hash

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const express = require("express")
 
 const app = express()
 
+
+const revision = require('child_process')
+.execSync('git rev-parse HEAD')
+.toString().trim()
+
+
+
 app.get("/",(req,res)=>{
-    res.send({message:"Hello from alestic beanstalk"})
+    res.send({message:"Hello from alestic beanstalk",latestCommitHash:revision})
 })
 const port = process.env.port || 3000
 app.listen(port,()=>{
