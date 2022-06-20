fd61c2b11b1d6d898a01afdde779cefd21e9d8a6
Ákos <akos.paska@risingstack.com>
2022-06-20 13:18:53 +0200

    node and git init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express()
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send({message:"Hello from alestic beanstalk"})
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Wazzup")
+});
+
+
+
+
+
