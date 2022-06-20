2788bd8c16a9b16d6871fd41eb6f6d4441818596
Ákos <akos.paska@risingstack.com>
2022-06-20 14:28:01 +0200

    port added

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
+require('dotenv').config()
+
 const express = require("express")
 
 const app = express()
 
-
-
-
 app.get("/",(req,res)=>{
     res.send({message:"Hello from alestic beanstalk"})
 })
-
-
-
-
-app.listen(3000,()=>{
-    console.log("Wazzup")
+const port = process.env.port || 3000
+app.listen(port,()=>{
+    console.log("Wadzzup")
+    console.log(`Ebs example listening on ${port}`)
 });
 
 
